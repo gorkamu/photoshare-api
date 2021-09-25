8e9b8950e4103503a969af7cee415c608a2ae7a3
gorkamu <gorkamu@hotmail.com>
2021-09-25 19:13:55 +0200

    Added sorting

--- resolvers/query.js
+++ resolvers/query.js
@@ -5,6 +5,25 @@ const { users, photos, tags} = require('../data/data');
 module.exports = {
     Query: {
         totalPhotos: () => photos.length,
-		allPhotos: () => photos
+		allPhotos: (_, { sort }, ctx) => {
+            if(sort === undefined) {
+                sort = {
+                    dir: "ASC",
+                    by: "id"
+                }
+            }
+
+            let ordered = photos.sort((a, b) => {
+                if(sort.dir === "DESC") return a[sort.by] > b[sort.by] ? -1 : 1
+                else return a[sort.by] > b[sort.by] ? 1 : -1
+            });
+
+
+            return ordered;
+        },
+        saluda: (_, {nombre}, ctx) => {
+            return `Hola ${nombre}`;
+        }
+
     }
 };
